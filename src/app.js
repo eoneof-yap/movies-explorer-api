1a1d3aef8bf79cfb6539c03f61925405ccc33c16
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-05 00:42:49 +0300

    feat: add basic server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import process from 'process';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const {
+  NODE_ENV = 'production',
+  DB_PATH = 'mongodb://127.0.0.1:27017/moviexdb',
+  PORT = 3000,
+} = process.env;
+
+mongoose.connect(DB_PATH);
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
+
+process.on('uncaughtException', (err, origin) => {
+  // TODO: replace console log with error handler
+  // eslint-disable-next-line no-console
+  console.log(
+    `Необработанная ошибка: "${origin}" "${err.name}" "${err.message}"`,
+  );
+});

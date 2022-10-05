e1c9917c5f4e83b8411d73b34cc7a5c09345163e
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-05 17:52:29 +0300

    refactor: move routes' paths to constants

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,17 @@
 import express from 'express';
+
+import { USER_PATH, MOVIES_PATH } from '../utils/constants.js';
 import {
   deleteMovieById, getMovieList, getUserInfo, updateMovieList, updateUserInfo,
 } from '../controllers/controllers.js';
 
 const routes = express();
 
-routes.get('/users/me', getUserInfo);
-routes.patch('/users/me', updateUserInfo);
-routes.get('/movies', getMovieList);
-routes.post('/movies', updateMovieList);
-routes.delete('/movies/:id', deleteMovieById);
+routes.get(USER_PATH, getUserInfo)
+  .patch(USER_PATH, updateUserInfo);
+
+routes.get(MOVIES_PATH, getMovieList)
+  .post(MOVIES_PATH, updateMovieList)
+  .delete(`${MOVIES_PATH}/:id`, deleteMovieById);
 
 export default routes;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
 // usrer schema strings
 export const USER_NAME_MIN_TXT = 'Имя должно быть длиной не менее двух символов';
 export const USER_NAME_MAX_TXT = 'Имя не должно быть длиннее 30 символов';
+
+export const USER_PATH = '/users/me';
+export const MOVIES_PATH = '/movies';

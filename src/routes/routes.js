c793c9735d2449bf91f6bd3cfbe1cc11eac3e6ef
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-06 12:30:45 +0300

    fix: rename other variables

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,16 @@ import express from 'express';
 
 import { USER_PATH, MOVIES_PATH } from '../utils/constants.js';
 import {
-  deleteMovieById, getMovieList, getUserInfo, updateMovieList, updateUserInfo,
+  deleteMovieById, getMovies, getUser, updateMovies, updateUser,
 } from '../controllers/controllers.js';
 
 const routes = express();
 
-routes.get(USER_PATH, getUserInfo)
-  .patch(USER_PATH, updateUserInfo);
+routes.get(USER_PATH, getUser)
+  .patch(USER_PATH, updateUser);
 
-routes.get(MOVIES_PATH, getMovieList)
-  .post(MOVIES_PATH, updateMovieList)
+routes.get(MOVIES_PATH, getMovies)
+  .post(MOVIES_PATH, updateMovies)
   .delete(`${MOVIES_PATH}/:id`, deleteMovieById);
 
 export default routes;

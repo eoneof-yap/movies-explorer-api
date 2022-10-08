caab4054243bb8924c709ec41d7d8dc80ee93b4d
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-08 22:15:49 +0300

    test: add getUser test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,11 @@
+import supertest from 'supertest';
+
 import {
   describe, test, expect, jest, beforeAll, afterAll, afterEach,
 } from '@jest/globals';
-import supertest from 'supertest';
 
 import * as db from './__utils__/virtualMongoServer.js';
-import { REGISTER_PATH } from '../src/utils/constants.js';
+import { CURRENT_USER_PATH, REGISTER_PATH } from '../src/utils/constants.js';
 import app from '../src/app.js';
 
 jest.setTimeout(30000);
@@ -16,38 +17,65 @@ const payload = {
   password: 'test123test',
 };
 
-describe('POST /signup', () => {
-  beforeAll(async () => {
-    await db.connect();
-  });
-  afterEach(async () => {
-    await db.clearDatabase();
-  });
-  afterAll(async () => {
-    await db.closeDatabase();
-  });
+const expectedPayload = {
+  name: 'test',
+  email: 'test@test.com',
+  // TODO: hide password and '__v'
+  password: 'test123test',
+  _id: expect.any(String),
+  __v: expect.any(Number),
+};
 
-  test('Статус 201', async () => {
-    const response = await request.post(REGISTER_PATH)
-      .send(payload).set('Content-Type', 'application/json');
-    const res = response.toJSON();
-    // console.log(res);
-    expect(res.status).toBe(201);
-  });
+beforeAll(async () => {
+  await db.connect();
+});
+afterEach(async () => {
+  await db.clearDatabase();
+});
+afterAll(async () => {
+  await db.closeDatabase();
+});
 
-  test('Объект содержит', async () => {
-    const response = await request.post(REGISTER_PATH).send({
-      name: 'test',
-      email: 'test@test.com',
-      password: 'test123test',
+describe('Пользователь', () => {
+  describe('Регистрация (POST /signup)', () => {
+    test('Возвращает статус 201', async () => {
+      const response = await request
+        .post(REGISTER_PATH).send(payload)
+        .set('Content-Type', 'application/json');
+
+      const data = response.toJSON();
+
+      expect(data.status).toBe(201);
     });
-    const res = response.toJSON();
-    expect(JSON.parse(res.text)).toEqual({
-      name: expect.any(String),
-      email: expect.any(String),
-      password: expect.any(String),
-      _id: expect.any(String),
-      __v: expect.any(Number),
+
+    test('Созданный объект пользователя соответствует переданному', async () => {
+      const response = await request
+        .post(REGISTER_PATH).send(payload)
+        .set('Content-Type', 'application/json');
+
+      const data = response.toJSON();
+
+      expect(JSON.parse(data.text)).toEqual(expectedPayload);
+    });
+  });
+
+  describe('Данные пользователя (GET /users/me)', () => {
+    test('Находит пользователя по ID', async () => {
+      const createResponse = await request
+        .post(REGISTER_PATH).send(payload)
+        .set('Content-Type', 'application/json');
+
+      const createdData = createResponse.toJSON();
+      const user = JSON.parse(createdData.text);
+
+      const searchResponse = await request
+        .get(CURRENT_USER_PATH).send({ id: user._id })
+        .set('Content-Type', 'application/json');
+
+      const searchData = searchResponse.toJSON();
+      const instance = JSON.parse(searchData.text);
+
+      expect(instance._id).toEqual(user._id);
     });
   });
 });

17f5d7b15f615b2c49a09af59b9ef8377c42fea4
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-09 14:54:42 +0300

    chore: rename error message name

--- src/middlewares/globalErrorHandler.js
+++ src/middlewares/globalErrorHandler.js
@@ -3,8 +3,8 @@ import { SERVER_ERROR, SERVER_ERROR_TXT } from '../utils/constants.js';
 export default function globalErrorHandler(err, req, res, next) {
   const { statusCode = SERVER_ERROR, message } = err;
 
-  res.status(statusCode).send({
-    message: statusCode === SERVER_ERROR ? SERVER_ERROR_TXT : message,
+  res.status(statusCode).json({
+    error: statusCode === SERVER_ERROR ? SERVER_ERROR_TXT : message,
   });
 
   next();

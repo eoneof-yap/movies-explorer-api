2d8db554118fd403164e055dc4e4500b3a08c763
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-10 21:59:01 +0300

    fix: validate only id

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,11 +3,11 @@ import { errors } from 'celebrate';
 
 import { CURRENT_USER_PATH } from '../utils/constants.js';
 import { getUser, updateUser } from '../controllers/userController.js';
-import { validateUserCredentials, validateUserInfo } from '../middlewares/validators.js';
+import { validateId, validateUserInfo } from '../middlewares/validators.js';
 
 const userRoute = express();
 
-userRoute.get(CURRENT_USER_PATH, validateUserCredentials, getUser)
+userRoute.get(CURRENT_USER_PATH, validateId, getUser)
   .patch(CURRENT_USER_PATH, validateUserInfo, updateUser);
 
 userRoute.use(errors());

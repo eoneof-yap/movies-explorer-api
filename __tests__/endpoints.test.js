b946925b56479ffb5ba3d94598d4ec03d4e2f146
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-10 13:21:41 +0300

    test: update tests

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -21,8 +21,34 @@ const request = supertest(app);
 const createUser = () => request.post(REGISTER_PATH).send(userPayload).set('Content-Type', 'application/json');
 const createLongUser = () => request.post(REGISTER_PATH).send(longUserPayload).set('Content-Type', 'application/json');
 const createInvalidUser = () => request.post(REGISTER_PATH).send(invalidUserPayload).set('Content-Type', 'application/json');
-const createEmpty = () => request.post(REGISTER_PATH).send({}).set('Content-Type', 'application/json');
+const createEmptyUser = () => request.post(REGISTER_PATH).send({}).set('Content-Type', 'application/json');
 const getUser = (user) => request.get(CURRENT_USER_PATH).send({ id: user._id }).set('Content-Type', 'application/json');
+
+const login = () => request.post(LOGIN_PATH).send(loginPayload).set('Content-Type', 'application/json');
+
+const patchUser = (user) => request.patch(CURRENT_USER_PATH).send({
+  id: user._id,
+  name: editedUserPayload.name,
+  email: editedUserPayload.email,
+});
+
+const patchShortId = () => request.patch(CURRENT_USER_PATH).send({
+  id: '123', // too short
+  name: editedUserPayload.name,
+  email: editedUserPayload.email,
+});
+
+const patchNonHexId = () => request.patch(CURRENT_USER_PATH).send({
+  id: 'zxcvbnmasdfghjklqwertyui', // non hex
+  name: editedUserPayload.name,
+  email: editedUserPayload.email,
+});
+
+const patchNonExistandId = () => request.patch(CURRENT_USER_PATH).send({
+  id: '634214006c025e0ffe6aab46', // valid but does not exist
+  name: editedUserPayload.name,
+  email: editedUserPayload.email,
+});
 
 beforeAll(async () => {
   await db.connect();
@@ -39,7 +65,7 @@ afterEach(() => {
 });
 
 afterAll(async () => {
-  await db.clearDatabase();
+  // await db.clearDatabase();
   await db.closeDatabase();
 });
 
@@ -47,7 +73,6 @@ describe('Общее', () => {
   test('Обращение по несуществующему пути возвращает статус 404 (GET /fake-path)', async () => {
     const response = await request.get('/fake-path');
     const data = response.toJSON();
-
     expect(data.status).toBe(404);
   });
 
@@ -60,7 +85,6 @@ describe('Пользователь', () => {
     test('Создает пользователя и возвращает JSON и статус 201 (POST /signup)', async () => {
       const response = await createUser();
       const data = response.toJSON();
-
       expect(response.headers['content-type']).toMatch('application/json');
       expect(data.status).toBe(201);
 
@@ -68,30 +92,26 @@ describe('Пользователь', () => {
     });
 
     test('Попытка передать пустой объект возвращает статус 500 (POST /signup)', async () => {
-      const response = await createEmpty();
+      const response = await createEmptyUser();
       const data = response.toJSON();
-
       expect(data.status).toBe(500);
     });
 
     test('Попытка передать невалидные данные возвращает статус 400 (POST /signup)', async () => {
       const response = await createInvalidUser();
       const data = response.toJSON();
-
       expect(data.status).toBe(400);
     });
 
     test('Попытка передать слишком длинные строки возвращает статус 400 (POST /signup)', async () => {
       const response = await createLongUser();
       const { status } = response.toJSON();
-
       expect(status).toBe(400);
     });
 
     test('Попытка передать уже существующую почту возвращает статус 409 (POST /signup)', async () => {
       const response = await createUser();
       const { status } = response.toJSON();
-
       expect(status).toBe(409);
     });
 
@@ -101,12 +121,11 @@ describe('Пользователь', () => {
   });
 
   describe('Данные пользователя', () => {
-    test('Находит пользователя по ID  (GET /users/me)', async () => {
+    test('Находит пользователя по ID (GET /users/me)', async () => {
       const user = JSON.parse(process.env.USER);
       const response = await getUser(user);
       const searchData = response.toJSON();
       const { _id } = JSON.parse(searchData.text);
-
       expect(response.headers['content-type']).toMatch('application/json');
       expect(response.ok).toBeTruthy();
       expect(_id).toEqual(user._id);
@@ -114,34 +133,38 @@ describe('Пользователь', () => {
 
     test('Обновляет имя и почту (PATCH /users/me)', async () => {
       const user = JSON.parse(process.env.USER);
-      const response = await request
-        .patch(CURRENT_USER_PATH).send({
-          id: user._id,
-          name: editedUserPayload.name,
-          email: editedUserPayload.email,
-        })
-        .set('Content-Type', 'application/json');
+      const response = await patchUser(user);
       const searchData = response.toJSON();
       const instance = JSON.parse(searchData.text);
-
       expect(response.headers['content-type']).toMatch('application/json');
       expect(response.ok).toBeTruthy();
-
       expect(instance.name).toEqual(editedUserPayload.name);
       expect(instance.email).toEqual(editedUserPayload.email);
+    });
+
+    test('Попытка передать невалидный id возвращает статус 400 (PATCH /users/me)', async () => {
+      const response = await patchNonHexId();
+      expect(response.status).toBe(400);
+    });
+
+    test('Попытка передать короткий id возвращает статус 400 (PATCH /users/me)', async () => {
+      const response = await patchShortId();
+      expect(response.status).toBe(400);
+    });
+
+    test('Попытка передать несуществующий id возвращает статус 404 (PATCH /users/me)', async () => {
+      const response = await patchNonExistandId();
+      expect(response.status).toBe(404);
     });
   });
 
   describe('Логин', () => {
     test('Логин  (POST /signin)', async () => {
       await createUser();
-      const response = await request.post(LOGIN_PATH).send(loginPayload).set('Content-Type', 'application/json');
+      const response = await login();
       const data = response.toJSON();
       const { token } = JSON.parse(data.text);
       expect(response.headers['content-type']).toMatch('application/json');
-
-      console.log(token);
-
       expect(typeof token).toBe('string');
     });
   });

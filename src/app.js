5c234195dd77c4ead1774aaeb66e672821d0e6d1
Nikolay Ivanov <eoneof@yandex.ru>
2022-10-14 17:35:52 +0300

    perf: improve logging

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import process from 'process';
 import { errors } from 'celebrate';
+// import cookieParser from 'cookie-parser'; TODO
 
 import getVirtualDbInstance from '../__tests__/utils/testHelpers.js';
 
-import { requestLogger } from './middlewares/loggers.js';
-import notFound from './controllers/notFoundController.js';
+import {
+  logRequestsToFile, logErrorsToFile, logRequestsToConsole, logErrosToConsole,
+} from './middlewares/loggers.js';
 
 import authRoute from './routes/authRoute.js';
 import userRoute from './routes/userRoute.js';
 import validateToken from './middlewares/validateToken.js';
 
-const { NODE_ENV = 'production' } = process.env;
+dotenv.config();
+const app = express();
 
+const { NODE_ENV = 'production' /* JWT_SECRET = '123-ABC-XYZ' */ } = process.env;
+
+if (NODE_ENV === 'production') {
+  app.use(logRequestsToFile);
+} else if (NODE_ENV === 'testing') {
 // connect to virtual DB while testing
-if (NODE_ENV === 'testing') {
   getVirtualDbInstance();
+} else {
+  app.use(logRequestsToConsole);
 }
 
 const app = express();
@@ -37,5 +47,12 @@ app.use(userRoute);
 // TODO: catch unauthorized 404s
 app.use(notFound); // 404
 app.use(errors()); // catch Joi validation errors
+
+if (NODE_ENV === 'production') {
+  app.use(logErrorsToFile);
+} else if (NODE_ENV === 'development') {
+  app.use(logErrosToConsole);
+}
+// app.all('*', notFound); // TODO fix 404 error handling
 
 export default app; // to server.js
